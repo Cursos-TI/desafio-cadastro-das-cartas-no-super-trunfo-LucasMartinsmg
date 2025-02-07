f737c4f6a1fc4134a8c7df3423ef198d5b65f655
LucasMartinsmg <lucasamptrader@gmail.com>
2025-02-07 02:05:55 +0000

    Variaveis cadastradas

---
+++ CartasSuperTrunfo2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+
+//CADASTRAMENTO DE VARIAVEIS
+
+char cidadeA , cidadeB , cidadeC , cidadeD , cidadeE, cidadeF, cidadeG, cidadeH;
+char codicoA01[5] ,  codicoA02[5] , codicoA03[5], codicoA04[5], codicoB01[5] ,  codicoB02[5] , codicoB03[5], codicoB04[5], codicoC01[5] ,  codicoC02[5] , codicoC03[5], codicoC04[5], codicoD01[5] ,  codicoD02[5] , codicoD03[5], codicoD04[5], codicoE01[5] ,  codicoE02[5] , codicoE03[5], codicoE04[5], codicoF01[5] ,  codicoF02[5] , codicoF03[5], codicoF04[5], codicoG01[5] ,  codicoG02[5] , codicoG03[5], codicoG04[5], codicoH01[5] ,  codicoH02[5] , codicoH03[5], codicoH04[5];
+char cdA01[10], cdA02[10], cdA03[10], cdA04[10], cdB01[10], cdB02[10], cdB03[10], cdB04[10], cdC01[10], cdC02[10], cdC03[10], cdC04[10], cdD01[10], cdD02[10], cdD03[10], cdD04[10], cdE01[10], cdE02[10], cdE03[10], cdE04[10], cdF01[10], cdF02[10], cdF03[10], cdF04[10], cdG01[10], cdG02[10], cdG03[10], cdG04[10], cdH01[10], cdH02[10], cdH03[10], cdH04[10];
+int PPA01, PPA02, PPA03, PPA04, PPB01, PPB02, PPB03, PPB04, PPC01, PPC02, PPC03, PPC04, PPD01, PPD02, PPD03, PPD04, PPE01, PPE02, PPE03, PPE04, PPF01, PPF02, PPF03, PPF04, PPG01, PPG02, PPG03, PPG04, PPH01, PPH02, PPH03, PPH04;
+float areaA01, areaA02, areaA03, areaA04, areaB01, areaB02, areaB03, areaB04, areaC01, areaC02, areaC03, areaC04, areaD01, areaD02, areaD03, areaD04, areaE01, areaE02, areaE03, areaE04, areaF01, areaF02, areaF03, areaF04, areaG01, areaG02, areaG03, areaG04, areaH01, areaH02, areaH03, areaH04;
+float PIBA01, PIBA02, PIBA03, PIBA04, PIBB01, PIBB02, PIBB03, PIBB04, PIBC01, PIBC02, PIBC03, PIBC04, PIBD01, PIBD02, PIBD03, PIBD04, PIBE01, PIBE02, PIBE03, PIBE04, PIBF01, PIBF02, PIBF03, PIBF04, PIBG01, PIBG02, PIBG03, PIBG04, PIBH01, PIBH02, PIBH03, PIBH04; 
+int PturisticoA01, PturisticoA02, PturisticoA03, PturisticoA04, PturisticoB01, PturisticoB02, PturisticoB03, PturisticoB04, PturisticoC01, PturisticoC02, PturisticoC03, PturisticoC04, PturisticoD01, PturisticoD02, PturisticoD03, PturisticoD04, PturisticoE01, PturisticoE02, PturisticoE03, PturisticoE04, PturisticoF01, PturisticoF02, PturisticoF03, PturisticoF04, PturisticoG01, PturisticoG02, PturisticoG03, PturisticoG04, PturisticoH01, PturisticoH02, PturisticoH03, PturisticoH04 ; 
+
+
+
+
+}
